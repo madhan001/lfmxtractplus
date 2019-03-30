bf7383713bd8e705156de3c80da4547f3272f557
madhan001 <madhanbalaji2000@gmail.com>
2019-03-30 15:18:52 +0530

    remove API keys

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 #spotify api credentials -- get yours from developer.spotify.com
 
-cid = '9a0b64c799a74024a89e598ea70263c1' #client id
-secret = 'b1bbe569f95949e09584be0905d5e028' #secret key
+cid =  #client id
+secret =  #secret key
 
 #lastfm credentials -- get yours from last.fm/api
 
-lfusername = 'madhan_001'#username
-lfkey = 'da70281f2f464cfaa4638c4bfe820f9a'#lfm api key
+lfusername = #username
+lfkey = #lfm api key
 
