61a496797f917349cb18d0b53ed55a9aaead4b86
madhan001 <madhanbalaji2000@gmail.com>
2019-03-30 16:06:16 +0530

    include config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+#spotify api credentials -- get yours from developer.spotify.com
+
+cid = #client id
+secret = #secret key
+
+#lastfm credentials -- get yours from last.fm/api
+
+lfusername = #username
+lfkey = #lfm api key
